8f77853355c1e37c60db68f688597d16b6c09c35
jcardenasdn <jcardenas@dichter-neira.com>
2022-02-15 17:53:05 -0500

    ajustes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MovieDetail from './components/MovieDetail';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -19,4 +20,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+serviceWorkerRegistration.register();
